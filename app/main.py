245545721b6b4d208dfbedb785209b487cc1d48e
Krish Patel <krish.patel2@somaiya.edu>
2026-03-05 16:31:57 +0530

    fix CORS for production

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI #This is what creates your entire web application.
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.github_routes import router as github_router
 
 app = FastAPI(title="ReviewPilot")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(github_router)
 
 @app.get("/")
 def root():
-    return {"status":"ReviewPilot API running"}
+    return {"status": "ReviewPilot API running"}
